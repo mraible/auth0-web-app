ca852b475e53372b7a4d694fef357daf7be81fc6
Matt Raible <matt.raible@okta.com>
2021-08-25 11:41:55 -0600

    Add Auth0 Middleware

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const path = require("path");
 const { createServer } = require("http");
-// 👉 Replace this with express-openid-connect require 👈
+const { auth, requiresAuth } = require("express-openid-connect");
 
 const {
   checkUrl,
@@ -37,7 +37,13 @@ app.use(
   })
 );
 
-// 👉 Replace this with auth middleware 👈
+app.use(
+  auth({
+     secret: SESSION_SECRET,
+     auth0Logout: true,
+     baseURL: APP_URL,
+  })
+);
 
 const expenses = [
   {
@@ -62,7 +68,7 @@ app.get("/", async (req, res) => {
 
 // 👇 add requiresAuth middlware to these private routes  👇
 
-app.get("/user", async (req, res) => {
+app.get("/user", requiresAuth(), async (req, res) => {
   res.render("user", {
     user: req.oidc && req.oidc.user,
     id_token: req.oidc && req.oidc.idToken,
@@ -71,7 +77,7 @@ app.get("/user", async (req, res) => {
   });
 });
 
-app.get("/expenses", async (req, res, next) => {
+app.get("/expenses", requiresAuth(), async (req, res, next) => {
   res.render("expenses", {
     user: req.oidc && req.oidc.user,
     expenses,
